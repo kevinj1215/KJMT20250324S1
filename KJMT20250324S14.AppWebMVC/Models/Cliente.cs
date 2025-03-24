fe0af0f6cead8f81121f2715b078467c45fb511f
Kevin Mendoza <kevinmentobar05@hotmail.com>
2025-03-24 13:57:21 -0600

    Se modifico el diseño del index borra crear editar y detalle

--- KJMT20250324S14.AppWebMVC/Models/Cliente.cs
+++ KJMT20250324S14.AppWebMVC/Models/Cliente.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace KJMT20250324S14.AppWebMVC.Models;
 
 public partial class Cliente
 {
+    [Display(Name = "Id")]
     public int Id { get; set; }
-
+    [Required(ErrorMessage = "El nombre del cliente es obligatorio.")]
     public string Nombre { get; set; } = null!;
-
+    [Display(Name = "Direcciòn")]
     public string? Direccion { get; set; }
-
+    [Display(Name = "Telèfono")]
     public string? Telefono { get; set; }
-
+    [Display(Name = "Email")]
     public string? Email { get; set; }
 }
