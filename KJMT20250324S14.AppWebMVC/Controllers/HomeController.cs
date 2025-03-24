e9b3854265a4232a53c51eb824526c26e16933d4
Kevin Mendoza <kevinmentobar05@hotmail.com>
2025-03-24 13:23:16 -0600

    Agregar archivos de proyecto.

---
+++ KJMT20250324S14.AppWebMVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using KJMT20250324S14.AppWebMVC.Models;
+
+namespace KJMT20250324S14.AppWebMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ KJMT20250324S14.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KJMT20250324S14.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
